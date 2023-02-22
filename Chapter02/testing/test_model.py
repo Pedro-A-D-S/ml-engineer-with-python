d71f7e536a00704e5406cddb2069d35e8ee78be5
Pedro Alves <pedroalves0409@gmail.com>
2023-02-22 07:07:35 -0300

    chapter 02

---
+++ Chapter02/testing/test_model.py
@@ -0,0 +1,8 @@
+import pytest
+from model import cluster_and_label
+from helper import get_taxi_data
+
+def test_cluster_and_label():
+    df = get_taxi_data()
+    results = cluster_and_label(df)
+    assert isinstance(results, dict)
